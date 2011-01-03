40a9c6fbd1260a0e7f7bf39d678bfbca4919526a
Franck Dernoncourt <franck.dernoncourt@gmail.com>
2011-01-03 01:04:19 +0100

    Random order for experiments

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ from illusions import exp3
 from illusions import exp4
 from userinput import *
 
-import os, pygame
+import os, pygame, random
 from pygame.locals import *
 
 
@@ -51,7 +51,7 @@ try:
     experiment_env["result_file"] = result_file
     experiment_env["separator"] = ','
     experiment_env["variant"] = ''
-    experiment_env["exp_duration"] = 5
+    experiment_env["exp_duration"] = 30
     experiment_env["id_answer"] = 0
     
     # Write result file's headers
@@ -59,32 +59,34 @@ try:
     
     
     # Call experiments
+    exp_map = {'exp1' : exp1.exp1, 'exp2' : exp2.exp2, 'exp3' : exp3.exp3, 'exp4' : exp4.exp4}
     
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1, 0.5, 1)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1, 1, 1)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1, 1.5, 1)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1, 1, 1)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1, 1, 2)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1, 1, 4)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 0.5, 1, 1)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1, 1, 1)
-    exp1.exp1(full_screen, experiment_env, surf, "fixed", 1.5, 1, 1)
+    exp_arg_list = [('exp1', [full_screen, experiment_env, surf, "fixed", 1, 0.5, 1]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 1, 1, 1]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 1, 1.5, 1]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 1, 1, 1]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 1, 1, 2]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 1, 1, 4]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 0.5, 1, 1]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 1, 1, 1]),
+                    ('exp1', [full_screen, experiment_env, surf, "fixed", 1.5, 1, 1]),
+                    ('exp1', [full_screen, experiment_env, surf, "same_dir"]),
+                    ('exp1', [full_screen, experiment_env, surf, "opp_dir"]),
+                    ('exp2', [full_screen, experiment_env, surf, "normal"]),
+                    ('exp3', [full_screen, experiment_env, surf, "object"]),
+                    ('exp3', [full_screen, experiment_env, surf, "hole"]),
+                    ('exp3', [full_screen, experiment_env, surf, "luminance_cycle"]),
+                    ('exp3', [full_screen, experiment_env, surf, "luminance_control"]),
+                    ('exp4', [full_screen, experiment_env, surf, "luminance_distortion"]),
+                    ('exp4', [full_screen, experiment_env, surf, "contraction_distortion"]),
+                    ('exp4', [full_screen, experiment_env, surf, "expansion_distortion"]),
+                    ('exp4', [full_screen, experiment_env, surf, "color_distortion"])]
     
-    exp1.exp1(full_screen, experiment_env, surf, "same_dir")
-    exp1.exp1(full_screen, experiment_env, surf, "opp_dir")
-    experiment_env["experiment_number"] = 2
-    exp2.exp2(full_screen, experiment_env, surf, "normal")
-    experiment_env["experiment_number"] = 3
-    exp3.exp3(full_screen, experiment_env, surf, "object")
-    exp3.exp3(full_screen, experiment_env, surf, "hole")
-    exp3.exp3(full_screen, experiment_env, surf, "luminance_cycle")
-    exp3.exp3(full_screen, experiment_env, surf, "luminance_control")
-    experiment_env["experiment_number"] = 4
-    exp4.exp4(full_screen, experiment_env, surf, "luminance_distortion")
-    exp4.exp4(full_screen, experiment_env, surf, "contraction_distortion")
-    exp4.exp4(full_screen, experiment_env, surf, "expansion_distortion")
-    exp4.exp4(full_screen, experiment_env, surf, "curvature_distortion")
-    exp4.exp4(full_screen, experiment_env, surf, "color_distortion")
+    random.shuffle(exp_arg_list)
+    
+    for exp_arg in exp_arg_list:
+        experiment_env["experiment_number"] = int(exp_arg[0][3])
+        exp_map[exp_arg[0]](*exp_arg[1])
     
     
 finally:
